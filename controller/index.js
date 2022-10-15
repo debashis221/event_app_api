257c50b61d31737cdd8224a4cbe3806a300e3c09
debashissaha21 <debashissaha221@gmail.com>
2022-10-15 12:49:01 +0530

    ;chnages

--- controller/index.js
+++ controller/index.js
@@ -6,6 +6,7 @@ const tabs_controller = require("./user/tabs.js");
 const events_controller = require("./events.js");
 const posts_controller = require("./posts.js");
 const gallery_controller = require("./gallery.js");
+const contact_controller = require("./contact.js");
 const permissiontypes_controller = require("./user/permissionTypes.js");
 const grouppermission_controller = require("./user/groupPermission.js");
 const userTypeCodes_controller = require("./user/userTypeCodes.js");
@@ -80,5 +81,6 @@ module.exports = function (app) {
   app.use("/api/v1/events", events_controller);
   app.use("/api/v1/posts", posts_controller);
   app.use("/api/v1/gallery", gallery_controller);
+  app.use("/api/v1/contact", contact_controller);
   app.use("/api/v1/userType", userTypeCodes_controller);
 };
