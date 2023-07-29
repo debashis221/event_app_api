91524089306671a83ff359741c60c84b2ecb1d81
Debashis Saha <debashissaha221@gmail.com>
2023-07-29 14:53:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const body_parser = require("body-parser");
 const route = require("./controller");
 const cors = require("cors");
 const app = express();
+const path = require('path')
 const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 const swagger_option = require("./config/swagger_options.json").options;
@@ -30,7 +31,7 @@ app.use(
 app.use(body_parser.json({ limit: "50mb", extented: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use("uploads", express.static("uploads"));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 app.use(function (err, req, res, next) {
   res.status(statusFiveHundred).send("Somthing broke !");
 });
